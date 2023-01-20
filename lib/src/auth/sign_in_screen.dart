9da2c58cce905ed5377dee7373acb164a0ccecfe
unknown <ricknogueira1231@gmail.com>
2023-01-20 17:14:44 -0300

    Adicionando o nome do app e animação das categorias

--- lib/src/auth/sign_in_screen.dart
+++ lib/src/auth/sign_in_screen.dart
@@ -1,3 +1,4 @@
+import 'package:animated_text_kit/animated_text_kit.dart';
 import 'package:flutter/material.dart';
 import 'package:greengrocer/src/auth/components/custom_text_field.dart';
 
@@ -10,8 +11,49 @@ class SignInScreen extends StatelessWidget {
       backgroundColor: Colors.green,
       body: Column(children: [
         Expanded(
-          child: Container(
-            color: Colors.blue,
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              const Text.rich(
+                TextSpan(
+                  style: TextStyle(fontSize: 40),
+                  children: [
+                    TextSpan(
+                      text: "Green",
+                      style: TextStyle(
+                        color: Colors.white,
+                        fontWeight: FontWeight.bold,
+                      ),
+                    ),
+                    TextSpan(
+                      text: "grocer",
+                      style: TextStyle(
+                        color: Colors.red,
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+              SizedBox(
+                height: 30,
+                child: DefaultTextStyle(
+                  style: const TextStyle(fontSize: 25),
+                  child: AnimatedTextKit(
+                    pause: Duration.zero,
+                    repeatForever: true,
+                    animatedTexts: [
+                      FadeAnimatedText(
+                        "Frutas",
+                      ),
+                      FadeAnimatedText("Verduras"),
+                      FadeAnimatedText("Legumes"),
+                      FadeAnimatedText("Carnes"),
+                      FadeAnimatedText("Laticíneos"),
+                    ],
+                  ),
+                ),
+              ),
+            ],
           ),
         ),
         Container(
