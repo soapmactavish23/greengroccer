348a24cf4a39266f2f0c386db53c834419613941
Henrick <ricknogueira1231@gmail.com>
2023-01-21 19:16:45 -0300

    AppBar com nome e ícone do carrínho

---
+++ lib/src/home/home_tab.dart
@@ -0,0 +1,42 @@
+import 'package:flutter/material.dart';
+import 'package:greengrocer/src/auth/config/custom_colors.dart';
+
+class HomeTab extends StatelessWidget {
+  const HomeTab({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Colors.transparent,
+        elevation: 0,
+        centerTitle: true,
+        title: Text.rich(
+          TextSpan(
+            style: const TextStyle(fontSize: 30),
+            children: [
+              TextSpan(
+                text: "Green",
+                style: TextStyle(color: CustomColors.customSwatchColor),
+              ),
+              TextSpan(
+                text: "grocer",
+                style: TextStyle(color: CustomColors.customContrastColor),
+              ),
+            ],
+          ),
+        ),
+        actions: [
+          IconButton(
+            onPressed: () {},
+            icon: Icon(
+              Icons.shopping_cart,
+              color: CustomColors.customSwatchColor,
+            ),
+          )
+        ],
+      ),
+      body: Container(),
+    );
+  }
+}
