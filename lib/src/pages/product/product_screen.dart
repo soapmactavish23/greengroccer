112de34ad6b96abc02cf56b312b1b14ee4a05619
unknown <ricknogueira1231@gmail.com>
2023-01-23 16:07:23 -0300

    Criação do widget de quantidade

--- lib/src/pages/product/product_screen.dart
+++ lib/src/pages/product/product_screen.dart
@@ -3,6 +3,7 @@ import 'package:flutter/material.dart';
 import 'package:greengrocer/src/config/custom_colors.dart';
 
 import 'package:greengrocer/src/models/item_model.dart';
+import 'package:greengrocer/src/pages/common_widgets/quantity_widget.dart';
 import 'package:greengrocer/src/services/utils_service.dart';
 
 class ProductScreen extends StatelessWidget {
@@ -56,11 +57,7 @@ class ProductScreen extends StatelessWidget {
                               ),
                             ),
                           ),
-                          Container(
-                            height: 30,
-                            width: 70,
-                            color: Colors.red,
-                          ),
+                          QuantityWidget(),
                         ],
                       ),
                       Text(
