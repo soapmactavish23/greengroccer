5bf554f3e91faa227675cbcc5a761a53f75eaa17
unknown <ricknogueira1231@gmail.com>
2023-01-24 16:33:55 -0300

    Total e confirmação de pedido

---
+++ lib/src/pages/cart/cart_tab.dart
@@ -0,0 +1,78 @@
+import 'package:flutter/material.dart';
+import 'package:greengrocer/src/config/custom_colors.dart';
+import 'package:greengrocer/src/services/utils_service.dart';
+
+class CartTab extends StatelessWidget {
+  CartTab({Key? key}) : super(key: key);
+
+  final UtilsService utilsService = UtilsService();
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Carrinho'),
+      ),
+      body: Column(
+        children: [
+          const Expanded(
+            child: Placeholder(
+              color: Colors.red,
+            ),
+          ),
+          const SizedBox(
+            height: 20,
+          ),
+          Container(
+            padding: const EdgeInsets.all(16),
+            decoration: BoxDecoration(
+              color: Colors.white,
+              borderRadius: const BorderRadius.vertical(
+                top: Radius.circular(30),
+              ),
+              boxShadow: [
+                BoxShadow(
+                    color: Colors.grey.shade300,
+                    blurRadius: 3,
+                    spreadRadius: 2),
+              ],
+            ),
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.stretch,
+              children: [
+                const Text(
+                  "Total geral",
+                  style: TextStyle(fontSize: 12),
+                ),
+                Text(
+                  utilsService.priceToCurreny(50.5),
+                  style: TextStyle(
+                      fontSize: 23,
+                      color: CustomColors.customSwatchColor,
+                      fontWeight: FontWeight.bold),
+                ),
+                SizedBox(
+                  height: 50,
+                  child: ElevatedButton(
+                    
+                    style: ElevatedButton.styleFrom(
+                      primary: CustomColors.customSwatchColor,
+                      shape: RoundedRectangleBorder(
+                        borderRadius: BorderRadius.circular(18),
+                      ),
+                    ),
+                    onPressed: () {},
+                    child: const Text(
+                      'Concluir Pedido',
+                      style: TextStyle(fontSize: 18),
+                    ),
+                  ),
+                )
+              ],
+            ),
+          )
+        ],
+      ),
+    );
+  }
+}
