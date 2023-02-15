ff17e1dd2de7a724a3efe451cc9a9c0b4e474e10
unknown <ricknogueira1231@gmail.com>
2023-02-15 13:44:57 -0300

    Navegação nomeada no app base

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
-import 'package:greengrocer/src/pages/auth/sign_in_screen.dart';
-import 'package:greengrocer/src/pages/splash/splash_screen.dart';
+import 'package:get/get.dart';
+import 'package:greengrocer/src/pages_routes/app_pages.dart';
 import 'package:intl/date_symbol_data_local.dart';
 
 void main() {
@@ -13,13 +13,14 @@ class MyApp extends StatelessWidget {
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
+    return GetMaterialApp(
       title: 'Flutter Demo',
       theme: ThemeData(
           primarySwatch: Colors.green,
           scaffoldBackgroundColor: Colors.white.withAlpha(190)),
       debugShowCheckedModeBanner: false,
-      home: const SplashScreen(),
+      initialRoute: PagesRoutes.splashRoute,
+      getPages: AppPages.pages,
     );
   }
 }
