b5f4b5ef5ee0913cc07b8047cd770241fc91e14f
unknown <ricknogueira1231@gmail.com>
2023-03-04 16:20:27 -0300

    Finalizando o HomeRepository

--- lib/src/pages/home/repository/home_repository.dart
+++ lib/src/pages/home/repository/home_repository.dart
@@ -1,15 +1,26 @@
 import 'package:greengrocer/src/constants/endpoints.dart';
+import 'package:greengrocer/src/models/category_model.dart';
+import 'package:greengrocer/src/pages/home/result/home_result.dart';
 import 'package:greengrocer/src/services/http_manager.dart';
 
 class HomeRepository {
   final HttpManager _httpManager = HttpManager();
-  getAllCategories() async {
+  Future<HomeResult> getAllCategories() async {
     final result = await _httpManager.restRequest(
       url: Endpoints.getAllCategories,
       method: HttpMethods.get,
     );
 
     if (result['result'] != null) {
-    } else {}
+      List<CategoryModel> data =
+          (result['result'] as List<Map<String, dynamic>>)
+              .map(CategoryModel.fromJson)
+              .toList();
+      return HomeResult<CategoryModel>.success(data);
+    } else {
+      return HomeResult.error(
+        "Ocorreu um erro inesperado ao recuperar as categorias",
+      );
+    }
   }
 }
